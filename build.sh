84b286c6408bb17c0421464b3cfca74d60daec39
odino <alessandro.nadalin@gmail.com>
2017-11-24 20:35:01 +0400

    tweaks + new binaries

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ echo "You have 5 seconds to stop this operation..."
 countdown 5
 
 sudo apt-get install -y --allow-unauthenticated \
-atom \
 build-essential \
 dropbox \
 flashplugin-installer \
@@ -42,7 +41,7 @@ countdown 5
 
 find . -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*' |  \
 sed 's/.\///' | \
-awk {'print "echo BUILDING DOCKER IMAGE " $1 " && docker build -t " $1 " " $1'} | \
+awk {'print "echo BUILDING DOCKER IMAGE " $1 " && sudo docker build -t " $1 " " $1'} | \
 bash
 
 echo "Manually installing atom's extensions:"
@@ -50,17 +49,33 @@ echo "You have 5 seconds to stop this operation..."
 
 countdown 5
 
-apm install docblockr atom-beautify nuclide
+apm install docblockr atom-beautify 
 
 echo "Configuring git:"
 echo "You have 5 seconds to stop this operation..."
 
 countdown 5
 
+git config core.fileMode false
 git config --global push.default simple
 git config --global core.editor vim
 git config --global user.name "odino"
 git config --global user.email "alessandro.nadalin@gmail.com"
 git config --global core.excludesfile ~/.gitignore_global
+
+echo "k8s ftw"
+
+cd /tmp
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.23.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
 
 echo 'All done!'
